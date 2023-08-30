2f27a5ab7457425a2c42ff80333cea16da4a1750
Suryakantdsa <dassuryakantadas@gmail.com>
2023-08-30 10:58:21 +0530

    fix some bug

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,14 @@ const crypto = require("crypto");
 const fs = require("fs");
 const path = require("path");
 const mongoose = require("mongoose");
-const Message = require("./models"); // Assuming your Message model is defined in a separate file
-const uri=`mongodb+srv://Suryakant:Suryadas@cluster0.mydbwj6.mongodb.net/EncryptApp?retryWrites=true&w=majority`
+const Message = require("./models"); 
+const uri = "mongodb+srv://Suryakant:Suryadas@cluster0.mydbwj6.mongodb.net/EncryptApp?retryWrites=true&w=majority";
 
 mongoose
-  .connect(uri)
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to MongoDB successfully");
   })
@@ -21,6 +24,18 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Load data from data.json
+const dataFilePath = path.join(__dirname, "data.json");
+const rawData = fs.readFileSync(dataFilePath);
+const { names, cities } = JSON.parse(rawData);
+
+// Encryption key (replace with your own)
+const encryptionKey = crypto.randomBytes(32).toString("hex"); // 32 bytes for AES-256
+
+// Generate random number within a range
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // Generate a hash of given fields
 function generateSecretKey(name, origin, destination) {
@@ -29,51 +44,45 @@ function generateSecretKey(name, origin, destination) {
   return hash.digest("hex");
 }
 
+// Encrypt data using AES-256-CTR
+function encryptData(data) {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv("aes-256-ctr", encryptionKey, iv);
+  const encryptedData = Buffer.concat([iv, cipher.update(data), cipher.final()]);
+  return encryptedData.toString("hex");
+}
+
 io.on("connection", (socket) => {
-  console.log("Client connected:", socket.id);
+  setInterval(async () => {
+    const encryptedMessages = [];
+    const numMessages = getRandomInt(49, 499);
 
-  socket.on("encryptedDataStream", async (encryptedDataStream) => {
-    const encryptedMessages = encryptedDataStream.split("|");
-
-    for (const encryptedMessage of encryptedMessages) {
-      try {
-        const decipher = crypto.createDecipher("aes-256-ctr", SECRET_KEY);
-        let decrypted = decipher.update(encryptedMessage, "hex", "utf8");
-        decrypted += decipher.final("utf8");
-
-        const decryptedMessage = JSON.parse(decrypted);
-        const { name, origin, destination, secret_key } = decryptedMessage;
-
-        const calculatedSecretKey = generateSecretKey(name, origin, destination);
-
-        if (secret_key === calculatedSecretKey) {
-          const newMessage = new Message({
-            name,
-            origin,
-            destination,
-            timestamp: new Date()
-          });
-
-          await newMessage.save();
-          console.log("Data saved to MongoDB:", decryptedMessage);
-
-          // Emit decoded data event
-          socket.emit("decodedData");
-        } else {
-          console.log("Data integrity compromised for:", decryptedMessage);
-        }
-      } catch (error) {
-        console.error("Error decrypting or saving data:", error);
-      }
+    for (let i = 0; i < numMessages; i++) {
+      const name = names[getRandomInt(0, names.length - 1)];
+      const origin = cities[getRandomInt(0, cities.length - 1)];
+      const destination = cities[getRandomInt(0, cities.length - 1)];
+      const secretKey = generateSecretKey(name, origin, destination);
+      const message = {
+        name,
+        origin,
+        destination,
+        secret_key: secretKey,
+      };
+      encryptedMessages.push(encryptData(JSON.stringify(message)));
     }
-  });
 
-  socket.on("disconnect", () => {
-    console.log("Client disconnected:", socket.id);
-  });
+    const encryptedStream = encryptedMessages.join("|");
+    console.log(encryptedStream)
+
+    try {
+      const savedMessages = await Message.insertMany(encryptedMessages);
+      socket.emit("savedMessages", savedMessages);
+    } catch (error) {
+      console.error("Error saving messages:", error);
+    }
+  }, 10000); // 10 seconds
 });
 
-
-server.listen(3001, () => {
+server.listen(3002, () => {
   console.log("Emitter service is running on port 3001");
 });
